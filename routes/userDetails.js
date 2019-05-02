a104153675a6a1df72e3e1996367a1c7b0274949
SHUBHAM GULATI <shubhamgulatipec@gmail.com>
2019-05-01 23:27:57 -0400

    adding changes

--- routes/userDetails.js
+++ routes/userDetails.js
@@ -4,6 +4,7 @@ var router = express.Router();
 router.post('/', function(request, response) {
 	var email = request.body.user.email;
 	
+	console.log(email);
 	if (email) {
 		global.connection.query('SELECT * FROM Users WHERE email = ?',
 		 [email], function(error, results, fields) {
