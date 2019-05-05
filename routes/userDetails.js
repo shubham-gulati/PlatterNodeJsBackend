c56ca9d96078cfb9302e55f6e19486314263962a
SHUBHAM GULATI <shubhamgulatipec@gmail.com>
2019-05-05 19:38:41 -0400

    changes user details api

--- routes/userDetails.js
+++ routes/userDetails.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-router.post('/', function(request, response) {
-	var email = request.body.user.email;
+router.get('/', function(request, response) {
+	//var email = request.body.user.email;
+	var email = request.query.email;
 	
-	console.log(email);
+	//console.log(email);
+	
 	if (email) {
 		global.connection.query('SELECT * FROM Users WHERE email = ?',
 		 [email], function(error, results, fields) {
